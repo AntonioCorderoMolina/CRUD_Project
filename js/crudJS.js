d94837716b526ee7dcfbc72b7a8ae7584198f8ff
Antonio Cordero Molina <acormol2001@g.educaand.es>
2024-01-26 19:40:49 +0100

    26/01/24

---
+++ js/crudJS.js
@@ -0,0 +1,13 @@
+/*********************************************************/
+/*      Style sheet for Crud porject website             */
+/*      author: Antonio Cordero Molina                   */
+/*      version: 2.0                                     */
+/*      comment: functions for the Crud porject website  */
+/*********************************************************/
+
+/************************* Script for the Login Page  **************************/
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+
