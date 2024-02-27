e0cc5ecb6d54cca6971dc21d257023162ac1a661
Antonio Cordero Molina <acormol2001@g.educaand.es>
2024-02-27 13:16:25 +0100

    27/02/24

--- js/crudJS.js
+++ js/crudJS.js
@@ -1,13 +1,44 @@
 /*********************************************************/
 /*      Style sheet for Crud porject website             */
 /*      author: Antonio Cordero Molina                   */
-/*      version: 2.0                                     */
+/*      version: 3.0                                     */
 /*      comment: functions for the Crud porject website  */
 /*********************************************************/
 
-/************************* Script for the Login Page  **************************/
+/********************************* LOGIN ***************************************/
+document.addEventListener("DOMContentLoaded", function() {
+    var loginForm = document.querySelector('.login-form');
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var usernameInput = document.getElementById('username');
+        var passwordInput = document.getElementById('password');
+        var username = usernameInput.value.trim();
+        var password = passwordInput.value.trim();
 
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+        if (username.length === 0 || username.length < 8 || username.length > 12) {
+            alert("The username must be between 8 and 12 characters.");
+            return;
+        }
+
+        if (password.length === 0 || password.length < 8 || password.length > 15) {
+            alert("The password must be between 8 and 15 characters.");
+            return;
+        }
+
+        if (!(/[A-Z]/.test(password) && /[a-z]/.test(password) && /[0-9]/.test(password))) {
+            alert("The password must contain at least one uppercase letter, one lowercase letter, and one digit.");
+            return;
+        }
+
+        // Si todas las validaciones pasan, enviar el formulario
+        loginForm.submit();
+
+        // Limpiar campos después de enviar el formulario
+        usernameInput.value = '';
+        passwordInput.value = '';
+    });
+});
+
+/******************************* INSERTION *************************************/
+
 
